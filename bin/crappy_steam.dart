420e6dde4249fbf5e2c4f1aba9d2c83011a256e1
Luca Coraci <luca.coraci@gmail.com>
2022-07-05 22:36:34 +0200

    initial commit

---
+++ bin/crappy_steam.dart
@@ -0,0 +1,10 @@
+import 'package:crappy_steam/crappy_steam.dart' as crappy_steam;
+
+const String version = "0.1";
+
+void main(List<String> arguments) {
+  print("======== Welcome to Crappy Steam =========");
+  print("                  v.$version");
+  print("==========================================");
+  print('Hello world: ${crappy_steam.calculate()}!');
+}
---
+++ test/crappy_steam_test.dart
@@ -0,0 +1,8 @@
+import 'package:crappy_steam/crappy_steam.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
